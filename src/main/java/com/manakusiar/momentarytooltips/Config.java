babf77e10eaf617bd6633ee17dc726b5798fb62b
Rune Warden <madium3@gmail.com>
2026-05-07 16:02:21 +0200

    Better config, changed how works

--- src/main/java/com/manakusiar/momentarytooltips/Config.java
+++ src/main/java/com/manakusiar/momentarytooltips/Config.java
@@ -1,27 +1,10 @@
 package com.manakusiar.momentarytooltips;
 
-//import java.util.List;
-//import java.util.Set;
-//import java.util.stream.Collectors;
-//
-//import net.minecraft.core.registries.BuiltInRegistries;
-//import net.minecraft.resources.ResourceLocation;
-//import net.minecraft.world.item.Item;
-//import net.neoforged.bus.api.SubscribeEvent;
-//import net.neoforged.fml.common.EventBusSubscriber;
-//import net.neoforged.fml.event.config.ModConfigEvent;
-import net.minecraft.ChatFormatting;
-import net.neoforged.fml.config.IConfigSpec;
+
 import net.neoforged.neoforge.common.ModConfigSpec;
 
-// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Neo's config APIs
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
-
-//    public static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
-//            .comment("Whether to log the dirt block on common setup")
-//            .define("logDirtBlock", true);
 
     public static final ModConfigSpec.BooleanValue MOD_ENABLED = BUILDER
             .comment("Toggle the mod.")
@@ -29,7 +12,31 @@ public class Config {
 
     public static final ModConfigSpec.BooleanValue SHOW_ENCHANTS = BUILDER
             .comment("Toggle if enchantment tooltips should always be shown.")
-            .define("showEnchants", true);
+            .define("showEnchants", false);
+
+    public static final ModConfigSpec.BooleanValue SHOW_ATTRIBUTES = BUILDER
+            .comment("Toggle if attributes (Attack damage, Attack speed) tooltips should always be shown.")
+            .define("showAttributes", false);
+
+    public static final ModConfigSpec.BooleanValue SHOW_LORE = BUILDER
+            .comment("Toggle if lore tooltips should always be shown.")
+            .define("showLore", false);
+
+    public static final ModConfigSpec.BooleanValue SHOW_DESCRIPTION = BUILDER
+            .comment("Toggle if descriptions tooltips should always be shown.")
+            .define("showDescription", false);
+
+    public static final ModConfigSpec.BooleanValue SHOW_INGREDIENTS = BUILDER
+            .comment("Toggle if ingredient tooltips mostly on smithing templates should always be shown.")
+            .define("showIngredients", false);
+
+    public static final ModConfigSpec.BooleanValue SHOW_UPGRADE = BUILDER
+            .comment("Toggle if upgrade tooltips on smithing upgrades should always be shown.")
+            .define("showUpgrade", false);
+
+    public static final ModConfigSpec.BooleanValue SHOW_APPLIES_TO = BUILDER
+            .comment("Toggle if 'Applies to:' mostly on smithing templates should always be shown.")
+            .define("showAppliesTo", false);
 
     public static final ModConfigSpec.BooleanValue SHOW_TOOLTIP_ICON = BUILDER
             .comment("Toggle the icon that appears when a item has a tooltip to show.")
@@ -39,22 +46,5 @@ public class Config {
             .comment("The icon that appears when a item has a tooltip to show. Remember about the optional space in front!")
             .define("tooltipIcon", " ⊽");
 
-//    public static final ModConfigSpec.IntValue MAGIC_NUMBER = BUILDER
-//            .comment("A magic number")
-//            .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
-//
-//    public static final ModConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
-//            .comment("What you want the introduction message to be for the magic number")
-//            .define("magicNumberIntroduction", "The magic number is... ");
-//
-//    // a list of strings that are treated as resource locations for items
-//    public static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
-//            .comment("A list of items to log on common setup.")
-//            .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), () -> "", Config::validateItemName);
-
     static final ModConfigSpec SPEC = BUILDER.build();
-
-//    private static boolean validateItemName(final Object obj) {
-//        return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
-//    }
 }
--- src/main/java/com/manakusiar/momentarytooltips/MomentaryTooltipsClient.java
+++ src/main/java/com/manakusiar/momentarytooltips/MomentaryTooltipsClient.java
@@ -2,12 +2,10 @@ package com.manakusiar.momentarytooltips;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.ItemEnchantments;
+import net.minecraft.network.chat.TextColor;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
@@ -17,6 +15,8 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
@@ -37,40 +37,106 @@ public class MomentaryTooltipsClient {
 
         // Mod working conditions
         if (!Screen.hasShiftDown() && Config.MOD_ENABLED.getAsBoolean()) {
-            // Basic
-            ItemStack stack = event.getItemStack();
             List<Component> tooltip = event.getToolTip();
-            int initialSize = tooltip.size();
+
+            if (tooltip.isEmpty()) return;
+
             Component itemName = tooltip.getFirst();
+            List<Component> attributes = new ArrayList<>();
+            List<Component> enchantments = new ArrayList<>();
+            List<Component> lore = new ArrayList<>();
+            List<Component> description = new ArrayList<>();
+            List<Component> appliesTo = new ArrayList<>();
+            List<Component> ingredients = new ArrayList<>();
+            List<Component> upgrade = new ArrayList<>();
 
-            event.getToolTip().clear();
-            event.getToolTip().add(itemName);
-
-            // Show enchant setting
-            boolean enchantsEnabled = Config.SHOW_ENCHANTS.getAsBoolean();
-            if (enchantsEnabled) {
-                ItemEnchantments enchantments = stack.getOrDefault(
-                        DataComponents.ENCHANTMENTS, ItemEnchantments.EMPTY);
-                for (var entry : enchantments.entrySet()) {
-                    tooltip.add(Enchantment.getFullname(entry.getKey(), entry.getIntValue()));
+            for (Component line : tooltip) {
+                if (Config.SHOW_ATTRIBUTES.getAsBoolean() &&
+                        checkForTranslationKey(line, "attribute.modifier", true) ||
+                        checkForTranslationKey(line, "attribute.name.", true)) {
+                    attributes.add(line);
                 }
-
-                ItemEnchantments storedEnchantments = stack.getOrDefault(
-                        DataComponents.STORED_ENCHANTMENTS, ItemEnchantments.EMPTY);
-                for (var entry : storedEnchantments.entrySet()) {
-                    tooltip.add(Enchantment.getFullname(entry.getKey(), entry.getIntValue()));
+                if (Config.SHOW_ATTRIBUTES.getAsBoolean() && checkForTranslationKey(line, "item.modifiers.", true)) {
+                    attributes.add(Component.empty());
+                    attributes.add(line);
+                }
+                if (Config.SHOW_ENCHANTS.getAsBoolean() && checkForTranslationKey(line, "enchantment.", true)) {
+                    enchantments.add(line);
+                }
+                if (Config.SHOW_UPGRADE.getAsBoolean() && checkForTranslationKey(line, ".upgrade", false)) {
+                    upgrade.add(line);
+                }
+                if (Config.SHOW_LORE.getAsBoolean() && isLoreLine(line)) {
+                    lore.add(line);
+                }
+                if (Config.SHOW_DESCRIPTION.getAsBoolean() && checkForTranslationKey(line, ".description", false)) {
+                    description.add(line);
+                }
+                if (Config.SHOW_APPLIES_TO.getAsBoolean() && checkForTranslationKey(line, ".applies_to", false)) {
+                    attributes.add(Component.empty());
+                    appliesTo.add(line);
+                }
+                if (Config.SHOW_INGREDIENTS.getAsBoolean() && checkForTranslationKey(line, ".ingredients", false)) {
+                    ingredients.add(line);
                 }
             }
 
-            int statusNumber = enchantsEnabled? 1 : 0;
-            if (event.getToolTip().size() < initialSize - 2 * statusNumber
-                    && Config.SHOW_TOOLTIP_ICON.getAsBoolean()) {
-                Component extraText = Component.literal(Config.TOOLTIP_ICON.get()).withStyle(ChatFormatting.DARK_GRAY);
-                MutableComponent extendedItemName = itemName.copy().append(extraText);
+            int initialSize = tooltip.size();
+
+            tooltip.clear();
+            tooltip.add(itemName);
+            tooltip.addAll(lore);
+            tooltip.addAll(description);
+            tooltip.addAll(upgrade);
+            tooltip.addAll(enchantments);
+            tooltip.addAll(attributes);
+            tooltip.addAll(appliesTo);
+            tooltip.addAll(ingredients);
+
+            if (Config.SHOW_TOOLTIP_ICON.getAsBoolean() && initialSize > tooltip.size()) {
+                MutableComponent finalName = itemName.copy();
+                finalName.append(Component.literal(Config.TOOLTIP_ICON.get()));
 
                 tooltip.removeFirst();
-                tooltip.addFirst(extendedItemName);
+                tooltip.addFirst(finalName);
             }
         }
+
+
+    }
+
+    // Check for all translation keys in a component
+    public static boolean checkForTranslationKey(Component component, String keyPrefix, boolean startsWith) {
+        // Check translatable contents
+        if (component.getContents() instanceof TranslatableContents translatable) {
+            if ((startsWith && translatable.getKey().startsWith(keyPrefix)) || (!startsWith && translatable.getKey().endsWith(keyPrefix))) {
+                return true;
+            }
+
+            // Check internal components
+            for (Object arg : translatable.getArgs()) {
+                if (arg instanceof Component argComponent) {
+                    if (checkForTranslationKey(argComponent, keyPrefix, startsWith)) return true;
+                }
+            }
+        }
+
+        // Check the components siblings
+        for (Component sibling : component.getSiblings()) {
+            if (checkForTranslationKey(sibling, keyPrefix, startsWith)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isLoreLine(Component component) {
+        TextColor color = component.getStyle().getColor();
+        if (color == null) return false;
+
+        boolean isDarkPurple = color.getValue() == ChatFormatting.DARK_PURPLE.getColor();
+
+        return component.getStyle().isItalic() && isDarkPurple;
     }
 }
